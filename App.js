16b9b6778dfecb8f073e32b08963794ed253725e
Edwin Aragón <ared2728@gmail.com>
2021-09-07 21:19:39 -0600

    Initial commit

--- App.js
+++ App.js
@@ -1,65 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-
-const donuts = [
-  {name: 'Mora', type: 'basic', key: 1},
-  {name: 'Chocolate', type: 'basic', key: 2},
-  {name: 'Azucar canela', type: 'basic', key: 3},
-  {name: 'Gansito', type: 'heavy', key: 4},
-  {name: 'Aleman', type: 'special', key: 5},
-  {name: 'Snicker', type: 'special', key: 6},
-  {name: 'Mazapan', type: 'special', key: 7},
-  {name: 'Oreo', type: 'special', key: 8},
-  {name: 'Azucar rellena manzana', type: 'special', key: 9},
-  {name: 'Chocolate rellena vainilla', type: 'special', key: 10},
-]
+import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-native';
 
 export default function App() {
   const [total, setTotal] = useState(0);
   const [isHomePage, setIsHomePage] = useState(true);
   const [selectedDonuts, setSelectedDonuts] = useState([]);
+  const [order, setOrder] = useState({});
+  const [donutName, setDonutName] = useState(null);
+  const [donutPrice, setDonutPrice] = useState(0);
   
-  const handleSumupBasic = () => setTotal(total + 13);
-  const handleSumupGourmet = () => setTotal(total + 16);
-  const handleSumupSpecial = () => setTotal(total + 19);
-  const handleSumupHeavy = () => setTotal(total + 29);
-  const sumupMapping = {
-    'basic': handleSumupBasic,
-    'special': handleSumupSpecial,
-    'heavy': handleSumupHeavy,
-  };
-  
-  const handleAddDonut = (donut) => {
-    if(selectedDonuts.some((d) => d.name === donut.name)) return;
+  const handleAddToOrder = (donut) => {
+    if (order[donut.key]) {
+      order[donut.key].count += 1;
+      setOrder({...order});
+    }else{
+      order[donut.key] = {
+        name: donut.name,
+        count: 1,
+      }
+      setOrder({...order});
+    }
+
+    setTotal(total + donut.price);
+  }
+
+  const handleRemoveFromOrder = (donut) => {
+    if (order[donut.key]) {
+      if (order[donut.key].count === 1) {
+        delete order[donut.key];
+      }else{
+        order[donut.key].count -= 1;
+      }
+      setOrder({...order});
+    }
     
-    selectedDonuts.push(donut);
-    setSelectedDonuts(selectedDonuts);
+    setTotal(total - donut.price);
+  }
+
+  const handleClearOrder = () => {
+    setTotal(0);
+    setOrder({});
+  }
+
+  const handleAddDonut = () => {
+    if(selectedDonuts.some((d) => d.name === donutName)) return;
+
+    const donut = {
+      name: donutName,
+      price: donutPrice,
+      key: selectedDonuts.length,
+    };
+
+    setSelectedDonuts([...selectedDonuts, donut]);
+    setDonutName('');
+    setDonutPrice('');
+  }
+
+  const handleRemoveDonut = (donut) => {
+    setSelectedDonuts(selectedDonuts.filter((d => d.key !== donut.key)))
   }
 
   if (isHomePage) {
     return (
-      <View>
-        <FlatList data={donuts} 
-            renderItem={({item}) => <Button title={item.name} onPress={() => handleAddDonut(item)}></Button>}></FlatList>
+      <View style={styles.homePageContainer}>
+        <TextInput style={styles.textInput} placeholder="Donut name" onChangeText={setDonutName}/>
+        <TextInput style={styles.textInput} placeholder="Donut price" onChangeText={setDonutPrice} keyboardType="number-pad" />
+        <Button title="Add" onPress={() => handleAddDonut()}></Button>
         <Button title="Next" onPress={() => setIsHomePage(false)}></Button>
         <FlatList data={selectedDonuts}
-            renderItem={({item}) => <Text>{item.name}</Text>}></FlatList>
+            renderItem={({item}) => <Button title={item.name} onPress={() => handleRemoveDonut(item)}
+            keyExtractor={(item, index) => index.toString()}></Button>}></FlatList>
       </View>
     );
   }else{
     return (
-      <View style={styles.container}>
+      <View>
         {
-          donuts.map((donut) => <Button key={donut.key} title={donut.name} onPress={sumupMapping[donut.type]}></Button>)
+          selectedDonuts.map((donut) => {
+            return(
+              <View key={donut.key}>
+                <Text>{donut.name}</Text>
+                <Button title="+" onPress={() => handleAddToOrder(donut)}></Button>
+                <Button title="-" onPress={() => handleRemoveFromOrder(donut)}></Button>
+              </View>
+            );
+          })
         }
-        <Text>{total}</Text>
-        <Button title="Clear" onPress={() => setTotal(0)}></Button>
+        <Text>The total is: ${total}</Text>
+        <Button title="Clear" onPress={() => handleClearOrder()}></Button>
+        <Text>The order:</Text>
+        <FlatList data={Object.values(order)}
+          renderItem={({item}) => <Text>{item.name} x{item.count}</Text>} 
+          keyExtractor={(item, index) => index.toString()}/>
         <Button title="Back" onPress={() => setIsHomePage(true)}></Button>
       </View>
     );  
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -68,4 +106,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  homePageContainer: {
+    flex: 1,
+    marginTop: 25,
+  },
+  textInput:{
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    borderStyle: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  }
 });
